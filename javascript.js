83b5d2813ee5217e460bbc8b661c5a8a8c4abd8b
Vanessa <vanessa.villegas88@gmail.com>
2025-01-31 16:33:13 -0800

    styling added
    added general style for alignment and font

---
+++ javascript.js
@@ -0,0 +1,4 @@
+//function for search using open weatherAPI
+//function replaces the following id items when submit button is clicked:
+// searching-city, city-result, current-date, weekday, month, day, year,hour, min,
+// temperature, windspeed, humidity,description,icon
